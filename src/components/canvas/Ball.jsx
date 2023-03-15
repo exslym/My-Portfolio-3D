378c88d0ca8a007262db45473d92f57ccc113733
exslym <76844562+exslym@users.noreply.github.com>
2023-03-16 01:30:25 +0400

    fixed Balls animation

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -21,7 +21,7 @@ const Ball = props => {
 
 const BallCanvas = ({ icon }) => {
 	return (
-		<Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+		<Canvas gl={{ preserveDrawingBuffer: true }}>
 			<Suspense fallback={<CanvasLoader />}>
 				<OrbitControls enableZoom={false} />
 				<Ball imgUrl={icon} />
