f2a90d2f8dd61856c5d576dea76614a4b9989bde
exslym <76844562+exslym@users.noreply.github.com>
2023-03-15 19:56:27 +0400

    Tech Section done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { technologies } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { styles } from '../styles/';
+import { BallCanvas } from './canvas';
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
-}
+	return (
+		<div className='flex flex-row flex-wrap justify-center gap-10'>
+			{technologies.map(technology => (
+				<div className='w-28 h-28' key={technology.name}>
+					<BallCanvas icon={technology.icon} />
+				</div>
+			))}
+		</div>
+	);
+};
 
-export default Tech
+export default SectionWrapper(Tech, '');
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import CanvasLoader from '../Loader';
 
-const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
-}
+const Ball = props => {
+	const [decal] = useTexture([props.imgUrl]);
 
-export default Ball
+	return (
+		<Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+			<ambientLight intensity={0.4} />
+			<directionalLight position={[0, 0, 0.05]} />
+			<mesh castShadow receiveShadow scale={2.75}>
+				<icosahedronGeometry args={[1, 1]} />
+				<meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
+				<Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.26]} flatShading map={decal} />
+			</mesh>
+		</Float>
+	);
+};
+
+const BallCanvas = ({ icon }) => {
+	return (
+		<Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls enableZoom={false} />
+				<Ball imgUrl={icon} />
+			</Suspense>
+
+			<Preload all />
+		</Canvas>
+	);
+};
+export default BallCanvas;
