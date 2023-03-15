405044a09651d67bcaa839b2b1b5624c18ed3a8c
exslym <76844562+exslym@users.noreply.github.com>
2023-03-15 13:53:39 +0400

    custom color added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
 				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex felx-row items-start gap-5`}
 			>
 				<div className='flex flex-col justify-center items-center mt-5'>
-					<div className='w-5 h-5 rounded-full bg-[#915eff] ' />
-					<div className='w-1 sm:h-80 h-40 violet-gradient ' />
+					<div className='w-5 h-5 rounded-full bg-[#58d5ff] ' />
+					<div className='w-1 sm:h-80 h-40 blue-gradient ' />
 				</div>
 				<div>
 					<h1 className={`${styles.heroHeadText} text-white `}>
-						Hi, I'm <span className='text-[#915eff]'>Andrei</span>
+						Hi, I'm <span className='text-[#58d5ff]'>Andrei</span>
 					</h1>
 					<p
 						className={`${styles.heroSubText} mt-2 text-white-100 md:max-w-[640px] max-w-[460px] `}
@@ -28,7 +28,7 @@ const Hero = () => {
 
 			<div className='absolute xs:bottom-8 bottom-32 w-full flex justify-center items-center '>
 				<a href='#about'>
-					<div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+					<div className='w-[35px] h-[64px] rounded-3xl border-4 border-custom1 flex justify-center items-start p-2'>
 						<motion.div
 							animate={{
 								y: [0, 24, 0],
@@ -38,7 +38,7 @@ const Hero = () => {
 								repeat: Infinity,
 								repeatType: 'loop',
 							}}
-							className='w-3 h-3 rounded-full bg-secondary mb-1 '
+							className='w-3 h-3 rounded-full bg-custom1 mb-1 '
 						/>
 					</div>
 				</a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
 			colors: {
 				primary: '#050816',
 				secondary: '#aaa6c3',
+				custom1: '#90a1b0',
 				tertiary: '#151030',
 				'black-100': '#100d25',
 				'black-200': '#090325',
